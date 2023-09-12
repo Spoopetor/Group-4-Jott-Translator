578d2a5a371b8e1cb42991ffefa5fb7559c2c2fe
m-dorsey <dorseymakenzie@gmail.com>
2023-09-12 01:18:26 -0400

    created while loop to parse through string of tokens and take no action for whitespace/comments

--- JottTokenizer.java
+++ JottTokenizer.java
@@ -7,8 +7,11 @@ package provided;
  **/
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class JottTokenizer {
+
+	public static final String NEWLINE = System.getProperty("line.separator");
 
 	/**
      * Takes in a filename and tokenizes that file into Tokens
@@ -17,6 +20,39 @@ public class JottTokenizer {
      * @return an ArrayList of Jott Tokens
      */
     public static ArrayList<Token> tokenize(String filename){
+
+		ArrayList<Token> tokens = new ArrayList<Token>();
+		ArrayList<String> charList = new ArrayList<String>(Arrays.asList(filename.split("")));
+
+		boolean commentFlag = false;
+		while (charList.size() > 0) {
+
+			String curr = charList.get(0);
+
+			if (commentFlag && !curr.equals(NEWLINE)) {
+				// throw out all characters within active comment
+				charList.remove(0);
+				continue;
+			}
+
+			if (curr.isBlank()) {
+				if (curr.equals(NEWLINE)) {
+					// upon new line, all potential line comments end
+					commentFlag = false;
+				}
+				// throw out any and all whitespace
+				charList.remove(0);
+				continue;
+
+			} else if (curr.equals("#")) {
+				// if #, line comment begins
+				commentFlag = true;
+			}
+
+			// remove character token from charList
+			charList.remove(0);
+		}
+
 		return null;
 	}
 }
