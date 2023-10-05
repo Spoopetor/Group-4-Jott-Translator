70e5aadc9f569b842f83976a23337b9832ff5597
uvu1688 <uvu1688@rit.edu>
2023-10-05 11:56:38 -0400

    Override JottTree methods in FuncParamsNode

--- nodes/FuncParamsNode.java
+++ nodes/FuncParamsNode.java
@@ -1,2 +1,30 @@
-package nodes;public class FuncParamsNode {
+package nodes;
+
+import provided.JottTree;
+
+public class FuncParamsNode implements JottTree {
+    @Override
+    public String convertToJott() {
+        return null;
+    }
+
+    @Override
+    public String convertToJava(String className) {
+        return null;
+    }
+
+    @Override
+    public String convertToC() {
+        return null;
+    }
+
+    @Override
+    public String convertToPython() {
+        return null;
+    }
+
+    @Override
+    public boolean validateTree() {
+        return false;
+    }
 }
