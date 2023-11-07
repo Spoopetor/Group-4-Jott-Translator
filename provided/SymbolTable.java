d1123daa9e745ba912659944059e07ed0948ac90
trl6895 <trl6895@g.rit.edu>
2023-11-07 00:05:43 -0500

    Created Symbol Table!!

--- provided/SymbolTable.java
+++ provided/SymbolTable.java
@@ -5,5 +5,55 @@ import java.util.HashMap;
 
 public class SymbolTable {
 
-    public static HashMap<String, ArrayList<Symbol>> scopeMap;
+    /** Hashmap where key is the name of the scope, and the value is the list of symbols in the scope */
+    public static HashMap<String, ArrayList<Symbol>> scopeMap = new HashMap<>();
+
+    /**
+     * adds a new symbol to a given scope
+     * @param scopeName the name of the scope to add to
+     * @param name the name of the symbol
+     * @param type the type of the symbol (Integer, Double, String, Boolean)
+     * @param value the value of the symbol
+     */
+    public static void addToScope(String scopeName, String name, Types type, String value){
+        Symbol newSymbol = new Symbol(name, type, value);
+        ArrayList<Symbol> scopeVars = scopeMap.get(scopeName);
+        if (scopeVars == null){
+            scopeVars = new ArrayList<>();
+        }
+        scopeVars.add(newSymbol);
+        scopeMap.put(scopeName, scopeVars);
+    }
+
+    /**
+     * create a new scope
+     * @param scopeName the name of the scope to create
+     */
+    public static void createScope(String scopeName){
+        scopeMap.put(scopeName, new ArrayList<Symbol>());
+    }
+
+    /**
+     * removes a scope (and therefore all symbols in the scope)
+     * @param scopeName the name of the scope to destroy
+     */
+    public static void destroyScope(String scopeName){
+        scopeMap.remove(scopeName);
+    }
+
+    /**
+     * Check if a symbol with the given name is in the given scope
+     * @param scopeName the name of the scope
+     * @param name the name of the symbol to look for
+     * @return true if present, false otherwise
+     */
+    public static boolean checkInScope(String scopeName, String name) {
+        ArrayList<Symbol> scopeSymbols = scopeMap.get(scopeName);
+        for (Symbol sym : scopeSymbols){
+            if (sym.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
